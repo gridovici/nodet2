490fc2417da86a9584cce6027abb50bbb64e9161
iradovic <iradovic@griddynamics.com>
2019-05-02 11:59:17 +0200

    Update .gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var app = express();
-var port = 3456;
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+const port = 3456;
+
+app.get('/', (req, res) => res.send('Hell World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
