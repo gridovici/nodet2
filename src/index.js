c3945c19408b5bde42db3b6fe8d22899d99e5253
iradovic <iradovic@griddynamics.com>
2019-05-11 02:22:40 +0200

    Add logging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3456;
 
+app.use(morgan('tiny'));
 app.get('/', (req, res) => res.send('Hell World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
