c9d2593b3b1278e8497db042890a4c1ea663bdfa
Igor Radovic <iradovic@griddynamics.com>
2019-07-17 01:20:17 +0200

    fix test lint error

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,7 +4,6 @@ const { expect } = chai;
 
 describe('smoke test', () => {
   it('checks equality', () => {
-    // eslint-disable-next-line no-unused-expressions
     expect(true).to.be.true;
   });
 });
