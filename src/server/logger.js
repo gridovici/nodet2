91a19a596500c10fc5d23122c3896b12dc67e761
Igor Radovic <iradovic@griddynamics.com>
2019-08-05 10:54:31 +0200

    fix logger message timestamp

--- src/server/logger.js
+++ src/server/logger.js
@@ -4,15 +4,17 @@ const { createLogger, format, transports } = require('winston');
 const fs = require('fs');
 const path = require('path');
 
-const env = process.env.NODE_ENV || 'development';
-// const logDir = 'log';
+// const env = process.env.NODE_ENV || 'development';
 const logDir = `${appRoot}/logs`;
 const filename = path.join(logDir, 'results.log');
+const filenameError = path.join(logDir, 'error.log');
 
 const options = {
   file: {
+    name: 'info-file',
+    //   // change level if in dev environment versus production
+    //   level: env === 'development' ? 'debug' : 'info',
     level: 'info',
-    // filename: `${logDir}/app.log`,
     filename,
     format: format.combine(
       format.timestamp({
@@ -26,10 +28,29 @@ const options = {
     maxFiles: 5,
     colorize: false
   },
+  fileError: {
+    name: 'error-file',
+    level: 'error',
+    filename: filenameError,
+    format: format.combine(
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      format.printf(info => `Error: ${info}`)
+    ),
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880,
+    maxFiles: 5,
+    colorize: false
+  },
   console: {
     level: 'debug',
     format: format.combine(
       format.colorize(),
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
       format.printf(
         info => `${info.timestamp} ${info.level}: ${info.message}`
       )
@@ -45,33 +66,11 @@ if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
-// const logger = createLogger({
-//   // change level if in dev environment versus production
-//   level: env === 'development' ? 'debug' : 'info',
-//   format: format.combine(
-//     format.timestamp({
-//       format: 'YYYY-MM-DD HH:mm:ss'
-//     }),
-//     format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-//   ),
-//   transports: [
-//     new transports.Console({
-//       level: 'info',
-//       format: format.combine(
-//         format.colorize(),
-//         format.printf(
-//           info => `${info.timestamp} ${info.level}: ${info.message}`
-//         )
-//       )
-//     }),
-//     new transports.File({ filename })
-//   ]
-// });
-
 class Logger {
   constructor() {
     this.logger = createLogger({
       transports: [
+        new transports.File(options.fileError),
         new transports.File(options.file),
         new transports.Console(options.console)
       ],
@@ -79,6 +78,7 @@ class Logger {
     });
     const self = this;
     this.logger.stream = {
+      // eslint-disable-next-line no-unused-vars
       write(message, encoding) {
         self.logger.info(message);
       }
